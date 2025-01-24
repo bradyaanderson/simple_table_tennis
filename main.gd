b195fd1908d7004a33306379b4bc8a4884e85532
Brady Anderson <brady@bradyanderson.tech>
2025-01-23 18:22:15 -0700

    Add score areas

--- main.gd
+++ main.gd
@@ -11,3 +11,13 @@ func reset_ball():
     var viewport_size = get_viewport().size
     var start_pos = viewport_size / 2
     $Ball.start_ball(start_pos)
+
+
+
+
+func _on_right_score_area_body_entered(body: Node2D) -> void:
+    print("Right Area Entered!")
+
+
+func _on_left_score_area_body_entered(body: Node2D) -> void:
+    print("Left Area Entered!")
